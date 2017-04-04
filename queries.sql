454806c7c759703b2ceea5f161cb773ea524a25a
yosirisandujar <yosiris.andujar@upr.edu>
2017-04-04 00:15:48 -0400

    Add files via upload

--- queries.sql
+++ queries.sql
@@ -36,11 +36,27 @@ SELECT *
     where email='example@domain.com'
     and password='password'
     and activeUser=true
-
+--Get administrator
+SELECT *
+	FROM public.person natural join public.administrator
+    where email='administrator@domain.com'
+    and password='password'
 	
---Verify if user is an administrator by email /priviled user/public user
-
-
+	
+--Verify if user is an administrator by email
+SELECT *
+	FROM public.person natural join public.administrator
+    where email='administrator@domain.com'
+----Verify if user is an priviled user by email
+SELECT *
+	FROM public.person natural join public.users
+    where email='example@domain.com'
+	and privilege=true
+----Verify if user is an public user by email
+SELECT *
+	FROM public.person natural join public.users
+    where email='example@domain.com'
+	and privilege=false
 
 --Remove User (set activeUser to false)
 UPDATE public.users
@@ -49,8 +65,59 @@ UPDATE public.users
                       FROM  public.person
                       WHERE email='example@domain.com')
 
+--Add privilege to user (set privilege to true)
+UPDATE public.users
+	SET privilege=true
+	WHERE personID in(select personID
+                      FROM  public.person
+                      WHERE email='example@domain.com')
 
+--Remove privilege from user (set privilege to true)
+UPDATE public.users
+	SET privilege=false
+	WHERE personID in(select personID
+                      FROM  public.person
+                      WHERE email='example@domain.com')
 	
+--Edit settings 
+--ALL
+--a.Change email
+UPDATE public.person
+	SET email='newemail@domain.com'
+	WHERE personID=1;
+--b.Change password
+UPDATE public.person
+	SET password='password1'
+	WHERE personID=1;
+--c.Change secretquestion
+UPDATE public.person
+	SET secretQuestion = '3'
+	WHERE personID=1;
+--d.Change secretAnswer
+UPDATE public.person
+	SET secretAnswer = 'newAnswer'
+	WHERE personID=1;
+--ADMINISTRATOR
+--a.securitykey
+UPDATE public.administrator
+	SET securitykey=1111
+	WHERE personID in(select personID
+                      FROM  public.person
+                      WHERE email='administrator@domain.com')
+--USER
+--a.organization
+UPDATE public.users
+	SET organization='DRNA1'
+	WHERE personID in(select personID
+                      FROM  public.person
+                      WHERE email='example@domain.com')
+--b.city
+UPDATE public.users
+	SET city='Arecibo'
+	WHERE personID in(select personID
+                      FROM  public.person
+                      WHERE email='example@domain.com')
+
 --Insert new River
 INSERT INTO public.rivers(
 	rivername, rivernumber, region, station, description)
